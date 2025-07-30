e6f5be24a7d1f463b290f3651207e2b194493480
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2025-07-30 19:12:03 -0400

    Added ADS1115 driver for onboarding (#1)

---
+++ libraries/ms-drivers/src/ads1115.c
@@ -0,0 +1,78 @@
+/************************************************************************************************
+ * @file    ads1115.c
+ *
+ * @brief   ADS1115 driver source file
+ *
+ * @date    2025-07-30
+ * @author  Midnight Sun Team #24 - MSXVI
+ ************************************************************************************************/
+
+/* Standard library Headers */
+
+/* Inter-component Headers */
+#include "ads1115.h"
+
+#include "gpio_interrupts.h"
+#include "i2c.h"
+
+/* Intra-component Headers */
+#include "status.h"
+
+StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAddress *ready_pin) {
+  if (config == NULL || ready_pin == NULL) {
+    return STATUS_CODE_INVALID_ARGS;
+  }
+
+  config->i2c_addr = i2c_addr;
+  uint16_t cmd;
+
+  /* --------------------- FW103 START --------------------- */
+  /* Configure for continuous mode (MODE bit = 0) */
+  cmd = 0x0000;
+
+  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
+
+  /* Configure lower threshold to be 0V */
+  cmd = 0x0000;
+  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
+
+  /* Configure higher threshold to be 1V */
+  cmd = 0x0000;
+  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_HI_THRESH, (uint8_t *)(&cmd), 2);
+  /* ---------------------- FW103 END ---------------------- */
+
+  // Register the ALRT pin
+  /* TODO (optional) */
+
+  return STATUS_CODE_OK;
+}
+
+StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channel) {
+  if (config == NULL) {
+    return STATUS_CODE_INVALID_ARGS;
+  }
+
+  uint16_t cmd;
+
+  /* --------------------- FW103 START --------------------- */
+  /* Configure command to select the requested channel */
+  cmd = 0x0000;
+  /* ---------------------- FW103 END ---------------------- */
+
+  i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
+  return STATUS_CODE_OK;
+}
+
+StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uint16_t *reading) {
+  /* --------------------- FW103 START --------------------- */
+  /* TODO: complete ADS1115 read raw function */
+  /* ---------------------- FW103 END ---------------------- */
+  return STATUS_CODE_OK;
+}
+
+StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
+  /* --------------------- FW103 START --------------------- */
+  /* TODO: complete ADS1115 read converted function */
+  /* ---------------------- FW103 END ---------------------- */
+  return STATUS_CODE_OK;
+}
