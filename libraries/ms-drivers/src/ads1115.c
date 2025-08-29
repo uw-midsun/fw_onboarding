4d6694d2f1d640bc84b862cdaf8eaeac992241d2
Aryan Kashem <aryankashem@gmail.com>
2025-08-29 15:55:56 -0400

    Revert solns

--- libraries/ms-drivers/src/ads1115.c
+++ libraries/ms-drivers/src/ads1115.c
@@ -28,20 +28,16 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   /* --------------------- FW103 START --------------------- */
   /* Configure for continuous mode (MODE bit = 0) */
-  cmd = 0x0483;
+  cmd = 0x0000;
 
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
-
-  /*
-   * Formula: (threshold_voltage) / (4.096V) * 2^15
-   */
 
   /* Configure lower threshold to be 0V */
   cmd = 0x0000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
-  /* Configure higher threshold to be 1V */
-  cmd = 7992;
+  /* Configure higher threshold to be 1.5V */
+  cmd = 0x0000;
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_HI_THRESH, (uint8_t *)(&cmd), 2);
   /* ---------------------- FW103 END ---------------------- */
 
@@ -70,12 +66,6 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uint16_t *reading) {
   /* --------------------- FW103 START --------------------- */
   /* TODO: complete ADS1115 read raw function */
-
-  if (NULL != config && NULL != reading) {
-    ads1115_select_channel(config, channel);
-    i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *)reading, sizeof(uint16_t));
-  }
-
   /* ---------------------- FW103 END ---------------------- */
   return STATUS_CODE_OK;
 }
@@ -83,17 +73,6 @@ StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, uin
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* --------------------- FW103 START --------------------- */
   /* TODO: complete ADS1115 read converted function */
-
-  uint16_t raw_reading = 0;
-  ads1115_read_raw(config, channel, &raw_reading);
-
-  if (NULL != reading) {
-    /*
-     * Formula: ((raw_reading) / (2^15)) * 4.096V
-     */
-    *reading = (((float)raw_reading) / (32767.0f)) * 4.096f;
-  }
-
   /* ---------------------- FW103 END ---------------------- */
   return STATUS_CODE_OK;
 }
