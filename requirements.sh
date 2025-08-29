fe25fbf9d59156502f34dd4824e20f9701213cd4
Aryan Kashem <130705280+Akashem06@users.noreply.github.com>
2025-08-29 12:28:31 -0400

    Fw 103 test run ak (#2)
    * Fix timing of RTOS task for onboarding
    
    * new install script
    
    * Fix workflow
    
    * Fix workflow
    
    * Fix requirements
    
    * Fix requirements

---
+++ requirements.sh
@@ -0,0 +1,61 @@
+add_line_if_dne () {
+  if [ ! grep -q "$1" "$2" ]; then
+    echo "$1" >> "$2"
+  fi
+}
+
+apt-get update
+
+echo "==> Install common tools"
+apt-get -y install tmux
+apt-get -y install git
+apt-get -y install vim
+apt-get -y install curl
+apt-get -y install scons
+
+echo "==> Install python tooling"
+apt-get -y install python3-pip
+apt-get -y install virtualenv
+apt-get -y install python3-autopep8
+apt-get -y install pylint
+python3 -m pip install cpplint
+
+echo "==> Install tooling for CAN"
+apt-get -y install can-utils
+
+python3 -m pip install python-can
+python3 -m pip install cantools
+python3 -m pip install Jinja2
+python3 -m pip install PyYAML
+python3 -m pip install pyserial
+
+echo "==> Install clang and gcc"
+apt install binutils-arm-none-eabi
+apt-get install -y libncurses5 libncursesw5
+add-apt-repository -y ppa:ubuntu-toolchain-r/test
+apt-get -y install gcc-11
+apt-get -y install g++-11
+apt-get -y install clang-10
+apt-get -y install clang-format-10
+apt-get -y install gdb
+
+ln -sf $(which gcc-11) /usr/bin/gcc
+ln -sf $(which clang-10) /usr/bin/clang
+ln -sf $(which clang-format-10) /usr/bin/clang-format
+
+if [ ! $? ]; then
+  echo "==> Install arm gcc 11.3"
+  wget https://developer.arm.com/-/media/Files/downloads/gnu/11.3.rel1/binrel/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi.tar.xz -O arm-gcc.tar.xz
+  tar xf arm-gcc.tar.xz -C /usr/local
+  add_line_if_dne 'PATH=$PATH:/usr/local/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi/bin' /etc/profile
+  rm arm-gcc.tar.xz
+fi
+
+echo "==> Install other toolchain pieces"
+apt-get -y install minicom
+apt-get -y install openocd
+
+echo "==> Setup for minicom"
+touch /etc/minicom/minirc.dfl
+add_line_if_dne "pu addcarreturn    Yes" /etc/minicom/minirc.dfl
+
