c642273315f71d95e2d654591d1d4bd3322b94f3
lukas1242434 <lukas.li.album@gmail.com>
2025-09-17 00:28:04 +0000

    first 102 submission

--- libraries/ms-drivers/src/ads1115.c
+++ libraries/ms-drivers/src/ads1115.c
@@ -28,17 +28,17 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   /* --------------------- FW103 START --------------------- */
   /* Configure for continuous mode (MODE bit = 0) */
-  cmd = 0x0000;
+  cmd = 0x0483;
 
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
   /* Configure lower threshold to be 0V */
   cmd = 0x0000;
-  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
+  i2c_write_reg(config->i2c_port, i2c_addr, 0, (uint8_t *)(&cmd), 2);
 
   /* Configure higher threshold to be 1.5V */
   cmd = 0x0000;
-  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_HI_THRESH, (uint8_t *)(&cmd), 2);
+  i2c_write_reg(config->i2c_port, i2c_addr, 24000, (uint8_t *)(&cmd), 2);
   /* ---------------------- FW103 END ---------------------- */
 
   // Register the ALRT pin
@@ -62,7 +62,8 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 
   /* --------------------- FW103 START --------------------- */
   /* Configure command to select the requested channel (Channel N should be default GND) */
-  cmd |= 0x0000U;
+  cmd |= 0x4000U;
+  cmd |= ((channel & 0x03) << 12);
   /* ---------------------- FW103 END ---------------------- */
 
   i2c_write_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
@@ -72,6 +73,7 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
   /* --------------------- FW103 START --------------------- */
   /* TODO: complete ADS1115 read raw function */
+	i2c_read_reg(config->i2c_port, config->i2c_addr, 0x00, &reading, 2);
   /* ---------------------- FW103 END ---------------------- */
   return STATUS_CODE_OK;
 }
@@ -79,6 +81,9 @@ StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* --------------------- FW103 START --------------------- */
   /* TODO: complete ADS1115 read converted function */
+	int16_t raw;
+	ads1115_read_raw(config, channel, &raw);
+	reading = raw/32768.0*2.048;
   /* ---------------------- FW103 END ---------------------- */
   return STATUS_CODE_OK;
 }
