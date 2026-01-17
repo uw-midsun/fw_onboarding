52b9addeb95de5008695173f608c23617747759a
Eric <ericnie12310@gmail.com>
2026-01-17 11:50:07 -0500

    FW102

--- projects/fw_102_103/src/main.c
+++ projects/fw_102_103/src/main.c
@@ -26,6 +26,25 @@
 
 static GpioAddress blinky_gpio = {
   /* --------------------- TODO: FW102 --------------------- */
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+static I2CSettings i2c_settings = {
+  .scl = { .port = GPIO_PORT_B, .pin = 7U },
+  .sda = { .port = GPIO_PORT_B, .pin = 6U },
+  .speed = I2C_SPEED_STANDARD
+};
+
+static GpioAddress ready_pin = {
+  .port = GPIO_PORT_B,
+  .pin = 0U,
+};
+
+static ADS1115_Config ads1115_cfg = {
+  .i2c_addr = ADS1115_ADDR_GND,
+  .i2c_port = ADS1115_I2C_PORT,
+  .ready_pin = &ready_pin,
 };
 
 static Queue ads1115_data_queue = {
@@ -42,6 +61,14 @@ TASK(blinky, TASK_STACK_256) {
 TASK(ads1115_writer, TASK_STACK_256) {
   /* --------------------- FW103 START --------------------- */
   /* This task will read from the ADS1115 external chip and push its data to a queue */
+  float reading = 0.0f;
+
+  while (true){
+    ads1115_read_converted(&ads1115_cfg, ADS1115_CHANNEL_0, &reading);
+    LOG_DEBUG("Voltage: %f\n", reading);
+    delay_ms(ADS1115_SAMPLING_PERIOD_MS);
+  }
+  
   /* --------------------- FW103 END --------------------- */
 }
 
@@ -75,6 +102,12 @@ TASK(ads1115_data_simulator, TASK_STACK_256) {
 int main() {
   /* --------------------- FW102 START --------------------- */
   /* Initialize the MCU, I2C, ADS1115 and blinky GPIO */
+  mcu_init();
+  gpio_init_pin(&blinky_gpio, GPIO_OUTPUT_PUSH_PULL, GPIO_STATE_LOW);
+
+  i2c_init(ADS1115_I2C_PORT, &i2c_settings);
+  ads1115_init(&ads1115_cfg, ADS1115_ADDR_GND, &ready_pin);
+
   /* --------------------- FW102 END --------------------- */
 
   /* Initialize printing module */
@@ -85,6 +118,7 @@ int main() {
 
   /* --------------------- FW103 START --------------------- */
   /* Initialize the RTOS tasks and data queue */
+  tasks_init_task(ads1115_writer, TASK_PRIORITY(1), NULL);
   /* --------------------- FW103 END --------------------- */
 
 #if defined(MS_PLATFORM_X86)
